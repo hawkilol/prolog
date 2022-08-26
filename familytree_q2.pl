5e3904faa6cdd14895f2edb8d2c5d11c3e9c4951
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-08-26 12:10:35 -0300

    Create familytree_q2.pl

---
+++ familytree_q2.pl
@@ -0,0 +1,41 @@
+homen(ivo).
+homen(gil).
+homen(rai).
+homen(ary).
+homen(noe).
+pai(ivo,eva).
+pai(rai,noe).
+pai(gil,rai).
+pai(gil,clo).
+pai(gil,ary).
+pai(ary,gal).
+mulher(ana).
+mulher(bia).
+mulher(eva).
+mulher(clo).
+mulher(lia).
+mulher(gal).
+mae(ana,eva).
+mae(eva,noe).
+mae(bia,clo).
+mae(bia,rai).
+mae(bia,ary).
+mae(lia,gal).
+
+gerou(X,Y):- pai(X,Y); mae(X,Y).
+filho(X,Y):- homen(X), gerou(Y,X).
+filha(X,Y):- mulher(X), gerou(Y,X).
+
+irmao(X,Y):- gerou(Z,X), pai(Z,Y), X\=Y. %irmao geral homen e mulher.
+
+irmão(X,Y) :- homen(X), irmao(X,Y).
+irma(X,Y) :- mulher(X), irmao(X,Y).
+
+tio(X,Y) :- gerou(Z,Y), irmão(X,Z).
+tia(X,Y) :- gerou(Z,Y), irma(X,Z).
+
+primo(X,Y) :- tio(Z,Y), filho(X,Z).
+prima(X,Y) :- tio(Z,Y), filho(X,Z).
+
+avô(X,Y) :- gerou(Z,Y), pai(X,Z).
+avó(X,Y) :- gerou(Z,Y), mae(X,Z).
