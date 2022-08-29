8f3888aacc7a47143b9200818fd4a13b68131192
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-08-29 16:13:12 -0300

    Update familytree_q2.pl

--- familytree_q2.pl
+++ familytree_q2.pl
@@ -94,4 +94,50 @@ avó(X,Y) :- gerou(Z,Y), mae(X,Z).
 feliz(X) :- gerou(X,Y).
 casal(X,Y) :- pai(X,Z), mae(Y,Z).
 
+%v3
+mulher(ana). %tirar talvez
+homen(ivo). %tirar talvez
+
+pai(ivo,eva).
+pai(rai,noe).
+pai(gil,rai).
+pai(gil,clo).
+pai(gil,ary).
+pai(ary,gal).
+mae(ana,eva).
+mae(eva,noe).
+mae(bia,clo).
+mae(bia,rai).
+mae(bia,ary).
+mae(lia,gal).
+
+homen(noe).
+mulher(clo).
+mulher(gal).
+
+homen(X):- pai(X,_).
+mulher(X):- mae(X,_).
+
+gerou(X,Y):- pai(X,Y).
+gerou(X,Y):- mae(X,Y).
+
+filho(X,Y):- homen(X), gerou(Y,X).
+filha(X,Y):- mulher(X), gerou(Y,X).
+
+irmao(X,Y):- gerou(Z,X), pai(Z,Y), X\=Y. %irmao geral homen e mulher.
+
+irmão(X,Y) :- homen(X), irmao(X,Y).
+irma(X,Y) :- mulher(X), irmao(X,Y).
+
+tio(X,Y) :- gerou(Z,Y), irmão(X,Z).
+tia(X,Y) :- gerou(Z,Y), irma(X,Z).
+
+primo(X,Y) :- tio(Z,Y), filho(X,Z).
+prima(X,Y) :- tio(Z,Y), filha(X,Z).
+
+avô(X,Y) :- gerou(Z,Y), pai(X,Z).
+avó(X,Y) :- gerou(Z,Y), mae(X,Z).
+
+feliz(X) :- gerou(X,Y).
+casal(X,Y) :- pai(X,Z), mae(Y,Z).
 
