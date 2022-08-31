2221822b662c4f57944e7d5cc0779558251a0913
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-08-31 11:58:13 -0300

    Update familytree_q2.pl

--- familytree_q2.pl
+++ familytree_q2.pl
@@ -1,102 +1,6 @@
-homen(ivo).
-homen(gil).
-homen(rai).
-homen(ary).
-homen(noe).
-pai(ivo,eva).
-pai(rai,noe).
-pai(gil,rai).
-pai(gil,clo).
-pai(gil,ary).
-pai(ary,gal).
-mulher(ana).
-mulher(bia).
-mulher(eva).
-mulher(clo).
-mulher(lia).
-mulher(gal).
-mae(ana,eva).
-mae(eva,noe).
-mae(bia,clo).
-mae(bia,rai).
-mae(bia,ary).
-mae(lia,gal).
-
-gerou(X,Y):- pai(X,Y); mae(X,Y).
-filho(X,Y):- homen(X), gerou(Y,X).
-filha(X,Y):- mulher(X), gerou(Y,X).
-
-irmao(X,Y):- gerou(Z,X), pai(Z,Y), X\=Y. %irmao geral homen e mulher.
-
-irmão(X,Y) :- homen(X), irmao(X,Y).
-irma(X,Y) :- mulher(X), irmao(X,Y).
-
-tio(X,Y) :- gerou(Z,Y), irmão(X,Z).
-tia(X,Y) :- gerou(Z,Y), irma(X,Z).
-
-primo(X,Y) :- tio(Z,Y), filho(X,Z).
-prima(X,Y) :- tio(Z,Y), filha(X,Z).
-
-avô(X,Y) :- gerou(Z,Y), pai(X,Z).
-avó(X,Y) :- gerou(Z,Y), mae(X,Z).
-
-feliz(X) :- gerou(X,Y).
-casal(X,Y) :- gerou(X,Z), gerou(Y,Z).
-
-
-%v2
-
-
-homen(ivo).
-homen(gil).
-homen(rai).
-homen(ary).
-homen(noe).
-pai(ivo,eva).
-pai(rai,noe).
-pai(gil,rai).
-pai(gil,clo).
-pai(gil,ary).
-pai(ary,gal).
-mulher(ana).
-mulher(bia).
-mulher(eva).
-mulher(clo).
-mulher(lia).
-mulher(gal).
-mae(ana,eva).
-mae(eva,noe).
-mae(bia,clo).
-mae(bia,rai).
-mae(bia,ary).
-mae(lia,gal).
-
-gerou(X,Y):- pai(X,Y).
-gerou(X,Y):- mae(X,Y).
-
-filho(X,Y):- homen(X), gerou(Y,X).
-filha(X,Y):- mulher(X), gerou(Y,X).
-
-irmao(X,Y):- gerou(Z,X), pai(Z,Y), X\=Y. %irmao geral homen e mulher.
-
-irmão(X,Y) :- homen(X), irmao(X,Y).
-irma(X,Y) :- mulher(X), irmao(X,Y).
-
-tio(X,Y) :- gerou(Z,Y), irmão(X,Z).
-tia(X,Y) :- gerou(Z,Y), irma(X,Z).
-
-primo(X,Y) :- tio(Z,Y), filho(X,Z).
-prima(X,Y) :- tio(Z,Y), filha(X,Z).
-
-avô(X,Y) :- gerou(Z,Y), pai(X,Z).
-avó(X,Y) :- gerou(Z,Y), mae(X,Z).
-
-feliz(X) :- gerou(X,Y).
-casal(X,Y) :- pai(X,Z), mae(Y,Z).
-
-%v3
-mulher(ana). %tirar talvez
-homen(ivo). %tirar talvez
+%Exercicio 1.4.
+mulher(ana). %não é necessário mas a questão pede
+homen(ivo). %não é necessário mas a questão pede
 
 pai(ivo,eva).
 pai(rai,noe).
@@ -114,13 +18,11 @@ mae(lia,gal).
 homen(noe).
 mulher(clo).
 mulher(gal).
-
 homen(X):- pai(X,_).
 mulher(X):- mae(X,_).
 
 gerou(X,Y):- pai(X,Y).
 gerou(X,Y):- mae(X,Y).
-
 filho(X,Y):- homen(X), gerou(Y,X).
 filha(X,Y):- mulher(X), gerou(Y,X).
 
@@ -138,6 +40,8 @@ prima(X,Y) :- tio(Z,Y), filha(X,Z).
 avô(X,Y) :- gerou(Z,Y), pai(X,Z).
 avó(X,Y) :- gerou(Z,Y), mae(X,Z).
 
-feliz(X) :- gerou(X,Y).
-casal(X,Y) :- pai(X,Z), mae(Y,Z).
+
+feliz(X) :- gerou(X,_).
+casal(X,Y) :- gerou(X,Z), gerou(Y,Z), X\=Y.
+
 
