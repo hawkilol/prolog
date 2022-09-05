22fa0f7f8bd9bd08f971574e49b8d9f90f5ee476
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-09-05 16:12:52 -0300

    Update q4.pl
    fera

--- q4.pl
+++ q4.pl
@@ -28,6 +28,41 @@ joga(leo,tenis).
 %a) ?- joga(P,X), joga(leo,X), P\=leo.
 %b) ?- joga(leo,X), joga(P,X), P\=leo. melhor ninja, menos reps e sem fail e redo.
 
+%3.2
 num(N, positivo):- N>0.
 num(0,nulo).
 num(N,negativo):- N<0.
+%Retrocesso desnecessario
+
+%Corte pula o retrocesso desnecessario:
+num2(N, positivo):- N>0,!.
+num2(N,negativo):- N<0, !.
+num2(0,nulo).
+
+num3(N, positivo):- N>0, !.
+num3(N, nulo):- N =:= 0, !.
+num3(N,negativo):- N<0.
+%macaco do futuro lembra de dar o trace!.
+
+%3.3
+%trace?
+falha :- (1=1).
+falha :- (1=2).
+
+%b) uma vez que ela sempre falha.
+
+%3.4
+animal(cão).
+animal(canário).
+animal(cobra).
+animal(morcego).
+animal(gaivota).
+voa(canário).
+voa(morcego).
+voa(gaivota).
+dif(X,X) :- !, fail.
+dif(_,_).
+
+
+pássaro(X) :- animal(X), voa(X), dif(X,morcego).
+%trace para pássaro(X).
