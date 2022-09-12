22f1d020f03ae54c44dfe7e029bc1c5bc5f74639
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-09-12 15:58:25 -0300

    Update q5.pl
    aaaaaaaaaaaaaaaaaaaaaaaa

--- q5.pl
+++ q5.pl
@@ -6,3 +6,19 @@ pot(B,N,P):-
 	pot(B,M,R),
 	P is B*R.
 
+%fat(Numero,Fatorial)
+
+fat(0,1).
+fat(N,F):-
+	N>0,
+	M is N-1,
+	fat(M,R),
+	F is N*R.
+
+pro2(_,0,0).
+pro2(X,Y,Z):-
+	Y>0,
+	M is Y-1,
+	pro2(X,M,R),
+	Z is X+R.
+
