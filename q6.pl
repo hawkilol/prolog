b3156d8c30cfc206ef1a6529806e527962eaa3e6
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-09-19 16:06:20 -0300

    Update q6.pl

--- q6.pl
+++ q6.pl
@@ -32,3 +32,34 @@ min([X],X).
 min([X|Y],X):- min(Y,M), X <= M.
 min([X|Y],M):- min(Y,M), X > M.
 
+%v2
+%5.3
+%append([], B, B).
+%append([X|A], B, [X|C]):- anexa(A, [B], C).
+
+%5.3
+%soma(L,S)
+soma([],0).
+soma([X|Y],S):- soma(Y,S1), S is S1 + X.
+
+%sim
+%5.4
+%max
+%5.5
+%anexa(A,B,C):
+anexa([], B, B).
+anexa([X|A], B, [X|C]):- anexa(A, B, C).
+
+%5.6
+inv([],[]).
+inv([X],[X]).
+inv([X|Y], YX):- inv(Y,Y1), anexa(Y1,[X],YX).
+
+%5.7
+d(0,zero).
+d(1,um).
+d(2,dois).
+
+txt([X|Y], [Xt|Yb]):- d(X, Xt), txt(Y,Yb).
+txt([],[]).
+
