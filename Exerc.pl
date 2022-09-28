aa356389851facf3627598b03d2d83eb91335b81
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-09-28 16:20:04 -0300

    Update Exerc.pl

--- Exerc.pl
+++ Exerc.pl
@@ -6,7 +6,7 @@
 pega3(E,[B,Y,E|D]):- E =x E.
 pega3(E,[B,Y,E|D]).
 pega3(E,[_,_,E|J]).
-%exec2
+%exerc2
 sum([],0).
 sum([X|Xs],X+S):- sum(Xs,S).
 sumAcc(L,S):- sum3(L,_,S). %?
@@ -15,3 +15,9 @@ sum3([X|X]).
 
 append1([],Bs,Bs).
 append1([A|As],Bs,[A|AsBs]):- append(As,Bs,AsBs).
+
+%exerc3
+membro(X,L):- append(_,[X|_],L).
+
+
+
