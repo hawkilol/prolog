a16e9e2ec5b8b82d66188d0f0297834dd5ffbad1
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-09-28 15:55:06 -0300

    Create Exerc.pl

---
+++ Exerc.pl
@@ -0,0 +1,11 @@
+%rega a)
+%usando a regra de fatoração, simplifique a clausula abaixo, indicando qual regra é utilizada em cada passo.
+%pega3(E,L):- L= [B|A], A = [Y|Ys], Ys = [Z|D], Z = E.
+%pega3(E,[B|[Y|[Z|D]]]):- Z = E.
+%pega3(E[B,Y,Z|D]):- Z = E.
+pega3(E,[B,Y,E|D]):- E =x E.
+pega3(E,[B,Y,E|D]).
+pega3(E,[_,_,E|J]).
+%exec2
+sum([],0).
+sum([X|Xs],X+S):- sum(Xs,S).
