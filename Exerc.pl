c525c1435d514488b9a1d210b9c299f3e0004298
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-10-03 15:44:12 -0300

    Update Exerc.pl

--- Exerc.pl
+++ Exerc.pl
@@ -48,6 +48,47 @@ prefix(P,L):- append(P,_,L).
 sufix(S,L):- append(_,S,L).
 sublist(S,L):- prefix(P,L), sufix(S,P).
 
+%%%%%
+%v2
+
+%1
+CompAcc(L,C):- comp3(L,O,C).
+comp3([X|X1],Acc,C):- comp3(X1,Acc+1,C).
+comp3([],Acc,C):- C = Acc.
+%2
+sum([],_).
+sum([X|X1],S+X):- sum(X1,S).
+
+sumAcc(L,S):- sum3(L,0,S).
+sum3([X|X1],Acc,S):- sum3(X1, Acc+X, S).
+sum3([], Acc, S):- S = Acc.
+
+%3
+membro(X,[X|_]).
+membro(X,[_|X1]):- membro(X,X1).
+
+%4
+%append([],B1,B2).
+append1([A|A1],B1,[A|A1B1]):- append1(A1,B1,A1B1).
+
+%5
+membro2(X,L):- append(_,[X|_],L).
+
+%6
+prefix(P,L):- append(P,_,L).
+sufix(S,L):- append(_,S,L).
+sublist(S,L):- prefix(P,L), sufix(S,P).
+
+%7
+%select1(X,[X|Xs],Xs).
+%select1(X,[Y|Xs],[Y|Zs]):- select1().
+
+%8
+%ultimo1(L,U):-L=[X|Xs],Xs=[],U=X,!.
+%ultimo1(L,U):- L=[X|Xs],X\= [], ultimo1([Xs,U],U),!.
+ultimo1([U],U):-!.
+ultimo1([_|Xs],U):- ultimo(Xs,U).
+
 
 
 
