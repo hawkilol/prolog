adab7b3de26e50afbed904acfcf63af4f2a6dc57
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-10-17 16:05:31 -0300

    Update q7.pl

--- q7.pl
+++ q7.pl
@@ -22,7 +22,25 @@ remDupl1([X|Xs], Xd):- member(X,Xs), select(X,Xs,Xd),
 remDupl1([X|Xs], [X|Xd]):- remDupl1(Xs, Xd).
 remDupl1([],[]).
 
+%20
 remove1(X,[X|Xs], Xr):- remove1(X, Xs, Xr).
 
 remove1(_,[],[]).
 remove1(X,[Y|Xs], [Y|Xr]):- X\=Y, remove1(X,Xs,Xr).
+
+
+%21
+flatten1([],[]).
+flatten1(X,[X]):- X\=[], X\=[_|_].
+flatten1([X|Xs], F):- flatten1(X,F1), flatten1(Xs, F2), append(F1,F2,F).
+
+%22
+
+zipper([X|Ys], [Y|Ys], [X-Y|XYs]):- zipper(Xs,Ys,XYs).
+zipper([],[],[]).
+
+%23
+permutation(Xs,[Z|Zs]):- select(Z,Xs,Ys), permutation(Ys,Zs).
+permutaion([],[]).
+
+%24
