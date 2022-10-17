2ae5a2e6189741f2bef7af7bef32d9698df7ef49
Kalil <67834605+hawkilol@users.noreply.github.com>
2022-10-17 15:26:10 -0300

    Update q7.pl

--- q7.pl
+++ q7.pl
@@ -1,1 +1,14 @@
+remU([_],[]):-!.
+remU([X|Xs],[X|Lu]):- remU(Xs, Lu).
 
+remU1(L,Lu):- append(Lu,[U],L).
+
+contiguos([X,X|_]):-!.
+contiguos([X|Xs]):-contiguos(Xs).
+
+contiguos1(L):- append(_,[X,X|_],L).
+
+duplicados([X|Xs]):- member(X,Xs),!.
+duplicados([_|Xs]):- duplicados(Xs).
+
+trocaPU(Lpu,Lup):- append(P,[U],Lpu), append(U,[P], Lup).
